918c7f9c333db8ab3809ea95e6355eac8f00701a
Momen Mahmoud <73801895+momen-mahmoud@users.noreply.github.com>
2023-08-22 17:33:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import json
+
+with open('C:\\Users\\momen\\Desktop\\json\\image1.txt', 'r') as txt_file:
+    lines = txt_file.readlines()
+
+
+json_data = {
+    "annotations": [
+        {
+            "result": []
+        }
+    ]
+}
+
+
+for line in lines:
+    values = line.strip().split()
+    if len(values) == 5:
+        x, y, width, height = map(float, values[1:])
+        annotation = {
+            "image_rotation": 0,
+            "value": {
+                "x": x ,
+                "y": y ,
+                "width": width ,
+                "height": height,
+                "rotation": 0,
+                "rectanglelabels": ["object"]
+            }
+        }
+        json_data["annotations"][0]["result"].append(annotation)
+
+json_data["data"] = {
+    "image": "C:\\Users\\momen\\Desktop\\json\\image1.jpg"
+}
+
+with open('image1.json', 'w') as json_file:
+    json.dump(json_data, json_file, indent=4)
